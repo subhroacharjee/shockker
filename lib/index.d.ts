f705add3a661d9c90e339bd2d4bb508f1bbb21b4
Subhro Acharjee <88422396+subhroacharjee@users.noreply.github.com>
2022-11-20 00:03:52 +0530

    adding changes

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,3 +1,5 @@
 import { SockerManager } from '@LIB/SockerManager';
+import { Router as r } from '@LIB/Router';
 declare const Socker: SockerManager;
+export declare const Router: typeof r;
 export default Socker;
